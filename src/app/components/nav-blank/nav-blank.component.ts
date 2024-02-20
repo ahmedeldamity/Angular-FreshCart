1a6b903d80708c7b954faf28669f3be0e0610f0a
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-02-20 21:14:04 +0200

    Navbar - footer

--- src/app/components/nav-blank/nav-blank.component.ts
+++ src/app/components/nav-blank/nav-blank.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-blank',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-blank.component.html',
   styleUrl: './nav-blank.component.scss'
 })
 export class NavBlankComponent {
+  constructor(private _Router:Router){}
+
+  signOut():void{
+    localStorage.removeItem('etoken');
+    this._Router.navigate(['/login']);
+  }
 
 }
